c52450c86032892043bce1960d6499944467e99a
Ariq Holland <001.aqh@gmail.com>
2019-06-23 08:00:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arrayNames, eventName){
+  let x = [];
+  for(let i=0; i<arrayNames.length; i++){
+    x.push(`Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return x;
+}
+
+function countdown(countdown){
+  while (countdown > 0){
+    console.log(countdown);
+    countdown-=1;
+  }
+  console.log(countdown);
+}
